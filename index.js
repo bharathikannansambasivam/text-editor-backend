77b2989570ecddf3b46e268545bd95d2fe17e4b1
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2025-09-12 20:29:55 +0530

    fix: add .env file

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const fileRouter = require("./src/routes/fileRoutes.js");
 const variableRouter = require("./src/routes/variableRoutes.js");
+require("dotenv").config();
 
-const { mongo, default: mongoose } = require("mongoose");
+const MONGO_URL = process.env.MONGO_URL;
+
+const { default: mongoose } = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(fileRouter);
 app.use(variableRouter);
 
 mongoose
-  .connect("mongodb://localhost:27017/file")
+  .connect(MONGO_URL)
   .then(() => {
     console.log("DB connected Successfully");
     app.listen(PORT, () => {
